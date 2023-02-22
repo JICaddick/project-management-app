f03f8101574cf53d9dbc8dbf38f7a409a815a343
JICaddick <james_caddick16@hotmail.com>
2023-02-21 22:27:28 -0500

    5 & 6. Comments added.

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,13 +1,12 @@
 import { MOCK_PROJECTS } from "./MockProjects";
+import ProjectList from "./ProjectList";
 //MOCK_PROJECTS is an array of objects, each object is a project. We're going to display the projects in a list and so we need to stringify the array of objects to display it in the browser. The second argument is a replacer function so we can pass null because we don't need to replace anything. The third argument is the number of spaces to use for indentation. We're going to use 2 spaces for indentation.
 
 export default function ProjectsPage() {
   return (
     <>
-      <pre>
-        <h1>Projects</h1>
-        {JSON.stringify(MOCK_PROJECTS, null, ' ')}
-      </pre>
+      <h1>Projects</h1>
+      < ProjectList projects={MOCK_PROJECTS} />
     </>
   );
 }
